b4a053e8777d6b4bd6728f99a91b10084287591a
xHao Wong <xHao Wong>
2016-06-28 22:22:33 +0800

    add 关于去除标题栏和状态跟随活动背景颜色的练习

--- app/src/main/java/com/xhao/practice/MainActivity.java
+++ app/src/main/java/com/xhao/practice/MainActivity.java
@@ -1,14 +1,23 @@
 package com.xhao.practice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         
     }
 
